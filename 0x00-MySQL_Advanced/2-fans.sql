3ca099f600af0bd7694f5fac41e98ba7fc9f42eb
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-25 13:10:33 +0000

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,16 @@
--- SQL script to rank country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, COUNT(fans) AS nb_fans
+-- Task: Rank country origins of bands by the number of (non-unique) fans
+-- All your files should start by a comment describing the task
+
+-- Create the necessary table by importing the provided SQL dump
+-- cat metal_bands.sql | mysql -uroot -p holberton;
+
+-- Your SQL queries should have a comment just before (i.e., syntax above)
+
+-- Rank country origins of bands by the number of (non-unique) fans
+-- and store the result in the 'tmp_res' file
+SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
 
+-- Note: The result will be stored in the 'tmp_res' file.
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,16 @@
--- 3-glam_rock.sql
--- script can be executed on any database
+-- Task: List all bands with Glam rock as their main style, ranked by their longevity
+-- All your files should start by a comment describing the task
 
-SELECT DISTINCT band_name,
-                IFNULL(`split`, 2020) - formed as lifespan
-    FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
-    ORDER BY `lifespan` DESC;
+-- Create the necessary table by importing the provided SQL dump
+-- cat metal_bands.sql | mysql -uroot -p holberton;
+
+-- Your SQL queries should have a comment just before (i.e., syntax above)
+
+-- List all bands with Glam rock as their main style, ranked by their longevity
+-- and display the band_name and lifespan (in years until 2022)
+SELECT
+    band_name,
+    IFNULL(YEAR(2022) - CAST(SUBSTRING_INDEX(splitted, '-', 1) AS UNSIGNED), 0) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', styles) > 0
+ORDER BY lifespan DESC, band_name;
