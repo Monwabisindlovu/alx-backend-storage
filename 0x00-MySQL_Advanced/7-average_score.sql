7dcd2ff3aced747629d17502ed59f8ab926e8e9f
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-25 09:52:26 +0000

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,22 +1,27 @@
--- Create Stored Procedure
+-- Create a stored procedure ComputeAverageScoreForUser
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN p_user_id INT
 )
 BEGIN
     DECLARE total_score FLOAT;
-    DECLARE total_count INT;
+    DECLARE total_projects INT;
 
-    -- Calculate total score and count of corrections for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, total_count
+    -- Calculate the total score and total number of projects for the user
+    SELECT SUM(score), COUNT(DISTINCT project_id)
+    INTO total_score, total_projects
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = p_user_id;
 
-    -- Calculate average score and update user's average_score attribute
-    IF total_count > 0 THEN
+    -- Calculate the average score and update the users table
+    IF total_projects > 0 THEN
         UPDATE users
-        SET average_score = total_score / total_count
-        WHERE id = user_id;
+        SET average_score = total_score / total_projects
+        WHERE id = p_user_id;
+    ELSE
+        UPDATE users
+        SET average_score = 0
+        WHERE id = p_user_id;
     END IF;
 END;
 //
