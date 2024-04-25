7913195446ddb4c6b981f5c6375bde243359e16b
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-25 09:16:32 +0000

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Create Trigger
+DELIMITER //
+CREATE TRIGGER reset_valid_email_on_email_change
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+//
+DELIMITER ;
+
