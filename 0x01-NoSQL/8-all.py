0820f5f34671055b24b511e2f6cbab8c682d9584
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-30 06:56:32 -0700

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+//8-all.py
+from pymongo import MongoClient
+
 def list_all(mongo_collection):
-    """List all documents in a collection"""
-    return list(mongo_collection.find())
+    """Function that lists all documents in a collection"""
+    documents = mongo_collection.find()
+    return [doc for doc in documents] if documents else []
