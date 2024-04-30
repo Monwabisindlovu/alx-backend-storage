3d76e780a0aa505545ad63cd87819acda1f7eb71
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-30 07:11:37 -0700

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" 101-students """
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+
+    students = mongo_collection.find()
+
+    # Calculate average score for each student
+    for student in students:
+        total_score = sum(topic['score'] for topic in student['topics'])
+        average_score = total_score / len(student['topics'])
+        student['averageScore'] = average_score
+
+    # Sort students by average score in descending order
+    sorted_students = sorted(students, key=lambda x: x['averageScore'], reverse=True)
+
+    return sorted_students
+
+# Test the function
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    students_collection = client.my_db.students
+
+    j_students = [
+        {'name': "John", 'topics': [{'title': "Algo", 'score': 10.3}, {'title': "C", 'score': 6.2}, {'title': "Python", 'score': 12.1}]},
+        {'name': "Bob", 'topics': [{'title': "Algo", 'score': 5.4}, {'title': "C", 'score': 4.9}, {'title': "Python", 'score': 7.9}]},
+        {'name': "Sonia", 'topics': [{'title': "Algo", 'score': 14.8}, {'title': "C", 'score': 8.8}, {'title': "Python", 'score': 15.7}]},
+        {'name': "Amy", 'topics': [{'title': "Algo", 'score': 9.1}, {'title': "C", 'score': 14.2}, {'title': "Python", 'score': 4.8}]},
+        {'name': "Julia", 'topics': [{'title': "Algo", 'score': 10.5}, {'title': "C", 'score': 10.2}, {'title': "Python", 'score': 10.1}]}
+    ]
+
+    for j_student in j_students:
+        insert_school(students_collection, **j_student)
+
+    students = list_all(students_collection)
+    for student in students:
+        print("[{}] {} - {}".format(student.get('_id'), student.get('name'), student.get('topics')))
+
+    top_students = top_students(students_collection)
+    for student in top_students:
+        print("[{}] {} => {}".format(student.get('_id'), student.get('name'), student.get('averageScore')))
