40171663d66441c803a609e8fc68bc7f5c254933
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-30 06:58:27 -0700

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
-//8-all.py
-from pymongo import MongoClient
+""" 8-all """
 
 def list_all(mongo_collection):
-    """Function that lists all documents in a collection"""
-    documents = mongo_collection.find()
-    return [doc for doc in documents] if documents else []
+    """List all documents in a collection"""
+    documents = []
+    for doc in mongo_collection.find():
+        documents.append(doc)
+    return documents
+
+# Test the function
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    
+    # Connect to MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    
+    # Access the collection
+    school_collection = client.my_db.school
+    
+    # List all documents in the collection
+    schools = list_all(school_collection)
+    
+    # Print the documents
+    for school in schools:
+        print("[{}] {}".format(school.get('_id'), school.get('name')))
